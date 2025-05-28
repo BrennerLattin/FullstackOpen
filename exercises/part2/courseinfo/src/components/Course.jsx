28f33114a4e18e0ebaf7e030f6486cf11ebfab51
Brenner Lattin <Brenner.Lattin@gmail.com>
2025-05-28 11:48:27 -0600

    Finish exercise 2.5: Separate module step 10

---
+++ exercises/part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,40 @@
+const Header = ({course}) => {
+  return (
+    <>
+    <h2>{course.name}</h2>
+    </>
+  )
+}
+
+const Content = ({course}) => {
+  return (
+    <>
+      {course.parts.map(part => <Part key={part.id} part={part} />)}
+    </>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Footer = ({course}) => {
+  const total = course.parts.reduce((accumulator, part) => accumulator + part.exercises, 0)
+
+  return (
+    <>
+      <p><b>Total of {total} exercises</b></p>
+    </>
+  )
+}
+
+const Course = ({ course }) =>
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Footer course={course} />
+  </div>
+
+export default Course
