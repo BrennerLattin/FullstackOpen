dd98af33f7104284d6cf6eed1e6d79821e4b93c2
Brenner Lattin <Brenner.Lattin@gmail.com>
2025-05-28 11:40:49 -0600

    Finish exercise 2.2: Course Information step 7

--- exercises/part2/courseinfo/src/App.jsx
+++ exercises/part2/courseinfo/src/App.jsx
@@ -25,7 +25,7 @@ const Footer = ({course}) => {
 
   return (
     <>
-      <p>Number of exercises {exercises.reduce((accumulator, exercises) => accumulator + exercises, 0)}</p>
+      <p><b>Total of {exercises.reduce((accumulator, exercises) => accumulator + exercises, 0)} exercises</b></p>
     </>
   )
 }
@@ -39,11 +39,29 @@ const Course = ({ course }) =>
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
-      {name: 'Fundamentals of React', exercises: 10},
-      {name: 'Using props to pass data', exercises: 7},
-      {name: 'State of a component', exercises: 14},
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
     ]
   }
 
