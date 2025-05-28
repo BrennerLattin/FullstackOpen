3f09f58e98d8148792e19346d0444776119c19b5
Brenner Lattin <Brenner.Lattin@gmail.com>
2025-05-28 11:38:25 -0600

    Finish exercise 2.1: Course Information step 6

---
+++ exercises/part2/courseinfo/src/App.jsx
@@ -0,0 +1,53 @@
+const Header = ({course}) => {
+  return (
+    <>
+    <h1>{course.name}</h1>
+    </>
+  )
+}
+
+const Content = ({course}) => {
+  return (
+    <>
+      {course.parts.map((part, index) => <Part key={index} part={part} />)}
+    </>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Footer = ({course}) => {
+  const exercises = course.parts.map(part => part.exercises)
+
+  return (
+    <>
+      <p>Number of exercises {exercises.reduce((accumulator, exercises) => accumulator + exercises, 0)}</p>
+    </>
+  )
+}
+
+const Course = ({ course }) =>
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Footer course={course} />
+  </div>
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name: 'Fundamentals of React', exercises: 10},
+      {name: 'Using props to pass data', exercises: 7},
+      {name: 'State of a component', exercises: 14},
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
