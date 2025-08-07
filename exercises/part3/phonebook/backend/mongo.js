e8fc11830a79e336b0969df961553ff7963ab48d
Brenner Lattin <Brenner.Lattin@gmail.com>
2025-08-07 11:34:24 -0600

    Finish exercise 3.12: Command-line database

---
+++ exercises/part3/phonebook/backend/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length != 3 && process.argv.length != 5) {
+    console.log('usage\n add: password name number\n view: password')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3] || ''
+const number = process.argv[4] || ''
+
+const url = `mongodb+srv://brennerlattin:${password}@fullstackopen.nwgcbv7.mongodb.net/phonebook?retryWrites=true&w=majority&appName=FullstackOpen`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name && number) {
+    const person = new Person({
+        name,
+        number
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${name} number ${number} to the phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(people => {
+        console.log('phonebook:')
+        people.forEach(person => console.log(`${person.name} ${person.number}`))
+        mongoose.connection.close()
+    })
+}
