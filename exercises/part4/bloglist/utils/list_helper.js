7657f2b01e2ba68f41c555b911b3e181ce07dbd3
Brenner Lattin <Brenner.Lattin@gmail.com>
2025-10-22 19:34:33 -0600

    Finish exercise 4.5: Helper Functions and Unit Tests, step 3

--- exercises/part4/bloglist/utils/list_helper.js
+++ exercises/part4/bloglist/utils/list_helper.js
@@ -3,6 +3,16 @@ const totalLikes = (blogs) => blogs.reduce(
   (sum, blog) => sum + blog.likes, 0
 )
 
+const favoriteBlog = (blogs) => {
+  let bestBlog = blogs[0]
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > bestBlog.likes) {
+      bestBlog = blogs[i];
+    }
+  }
+  return bestBlog
+}
+
 module.exports = {
-   totalLikes
+   totalLikes, favoriteBlog
 }
